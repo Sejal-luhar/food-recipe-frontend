5d4f9be99c0391be0f454295630aa9b811a5547f
Sejal-luhar <sejalluhar2001@gmail.com>
2024-12-05 21:30:10 +0530

    axios

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -3,9 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: 'https://food-recipe-backend-fug8.onrender.com',
   withCredentials:true,
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem('token')}`, // Include token if stored
-  },
+
 });
 
 export default axiosInstance;
